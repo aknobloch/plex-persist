e3ca2b7cf4cc2c9b2a342c77dcb8c026cb4f4a37
aknobloch <aaronknobloch92@gmail.com>
2019-03-25 19:44:39 -0400

    fix empty tag logic and grabbing art url

--- song.py
+++ song.py
@@ -4,26 +4,44 @@ class Song() :
 
     def __init__(self, track):
 
-        self.title = self.__get_tag__(track.title)
-        self.artist = self.__get_tag__(track.grandparentTitle)        
-        self.album = self.__get_tag__(track.parentTitle)
-        self.artwork_url = self.__get_tag__(track.thumbUrl)
+        self.title = self.__get_tag(track.title)
+        self.artist = self.__get_tag(track.grandparentTitle)
+        self.album = self.__get_tag(track.parentTitle)
+        self.artwork_url = self.__get_artwork_url(track)
 
         abs_file_location = track.media[0].parts[0].file
-        self.sys_location = self.__get_tag__(abs_file_location)
-        self.filetype = self.__get_tag__(abs_file_location[abs_file_location.rfind('.') + 1:].upper())
+        self.sys_location = self.__get_tag(abs_file_location)
+        self.filetype = self.__get_tag(abs_file_location[abs_file_location.rfind('.') + 1:].upper())
+
+    def __get_artwork_url(self, track) :
+        
+        if not self.is_empty(track.thumb) :
+            return track.url(track.thumb)
+
+        elif not self.is_empty(track.parentThumb) :
+            return track.url(track.parentThumb)
+            
+        else :
+            return self.__get_tag(track.url(track.grandparentThumb))
 
     '''
-    Checks if the given name is NoneType or if Plex
-    has labeled the tag with '[Unknown <tag type>]'.
-    If so, returns None. Otherwise, returns name.
+    Gets the tag name, or None if it is not tagged.
     '''
-    def __get_tag__(self, name) :
+    def __get_tag(self, name) :
 
-        if name is None or name.strip().startswith('[Unknown') :
+        if self.is_empty(name) :
             return None
         else :
             return name
+
+    '''
+    Checks if the given tag is NoneType or if Plex
+    has labeled the tag with '[Unknown <tag type>]'.
+    If so, returns true. Otherwise, returns false.
+    '''
+    def is_empty(self, tag) :
+
+        return tag is None or tag.strip().startswith('[Unknown')
 
     def __str__(self) :
 
