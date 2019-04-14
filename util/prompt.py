6fdd34b0e990f666005499ab9f39d408fbd894cf
aknobloch <aaronknobloch92@gmail.com>
2019-04-14 13:43:30 -0400

    fix abortion prompt

--- util/prompt.py
+++ util/prompt.py
@@ -1,9 +1,13 @@
+import sys
+from util.logutil import log
+
 def do_abort_prompt() :
 
     abort = ''
 
-    while abort != 'Y' or abort != 'N' :
-        abort = raw_input('Would you like to abort the process now? (y/n): ')
-    
+    while abort != 'Y' and abort != 'N' :
+        abort = input('Would you like to abort the process now? (y/n): ').upper()
+
     if abort == 'Y' :
+        log.info('Exiting...')
         sys.exit()
