a16a3d9b0e2cf9946d55ac5753ed000e3c293ad0
aknobloch <aaronknobloch92@gmail.com>
2019-04-14 13:21:40 -0400

    add abortion prompt on fatal exception

--- plex-persist.py
+++ plex-persist.py
@@ -97,6 +97,17 @@ def process_song(song) :
         writer.write_song_info_to_disk()
     except Exception :
         log.exception('Error writing some, or all, of the song info to disk!')
+        do_abort_prompt()
+
+def do_abort_prompt() :
+
+    abort = ''
+
+    while abort != 'Y' or abort != 'N' :
+        abort = raw_input('Would you like to abort the process now? (y/n): ')
+    
+    if abort = 'Y' :
+        sys.exit()
 
 def get_total_songs(music_collection) :
 
