6e9183a3161c09565aabfea772955395c59ea0f3
aknobloch <aarondevelops@gmail.com>
2019-07-24 20:57:23 -0400

    better CLI args

--- util/argparser.py
+++ util/argparser.py
@@ -4,18 +4,22 @@ def get_arg_parser() :
 
     parser = argparse.ArgumentParser(
         prog='Plex Persist',
-        usage='python3 persist.py MyServer Music aknobloch MY_P455W0RD',
+        usage='python3 plex-persist.py --name uServer --section Music --user aknobloch --password MY_P455W0RD',
         description='Persists the metadata information from a Plex Media Server.')
 
     parser.add_argument(
-        'server_name',
-        metavar='server',
+        '-n',
+        '--name',
+        required=True,
+        metavar='name',
         type=str,
         help='Name of the Plex server. This can be found by logging into \
                     the desired server via the Web UI. The name will be in the upper left.')
 
     parser.add_argument(
-        'section_name',
+        '-s',
+        '--section',
+        required=True,
         metavar='section',
         type=str,
         help='Name of the audio library section. This is usually \"Music\," \
@@ -24,20 +28,24 @@ def get_arg_parser() :
                     your libraries will be on the left panel.')
 
     parser.add_argument(
-        'username',
+        '-u',
+        '--user',
+        required=True,
         metavar='username',
         type=str,
         help='Username for the owner of the Plex server.')
 
     parser.add_argument(
-        'password',
+        '-p',
+        '--password',
+        required=True,
         metavar='password',
         type=str,
         help='Password for the owner of the Plex server.')
 
     parser.add_argument(
-        '--verbose',
         '-v',
+        '--verbose',
         action='store_true',
         help='Enables verbose logging.',
         dest='debug')
